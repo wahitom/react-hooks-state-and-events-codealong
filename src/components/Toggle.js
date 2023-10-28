f1c6b3651637a476ccfd7fd5948bebf4af14fd3e
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-10-28 16:26:50 +0300

    Completed

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setIsOn] = useState(false);
+
+  function handleClick() {
+    setIsOn((isOn) => !isOn);
+  }
+
+  const color = isOn ? "green" : "white";
+
+  return (
+    <>
+      <button style={{ background: color }} onClick={handleClick}>
+        {isOn ? "ON" : "OFF"}
+      </button>
+    </>
+  );
 }
 
 export default Toggle;
